ca330e4b847e1f3b624243abce818c9334ba7095
MrKumakun <159548828+MrKumakun@users.noreply.github.com>
2025-11-18 00:03:26 +0900

    Merge remote-tracking branch 'upstream/main'

--- ST.Library.UI/NodeEditor/STNode.cs
+++ ST.Library.UI/NodeEditor/STNode.cs
@@ -1011,7 +1011,7 @@ namespace ST.Library.UI.NodeEditor
         protected internal virtual void OnMouseWheel(MouseEventArgs e) {
             Point pt = e.Location;
             pt.Y -= this._TitleHeight;
-            if (m_ctrl_hover != null && m_ctrl_active != null && m_ctrl_active.Enabled && m_ctrl_hover.Visible) {
+            if (m_ctrl_hover != null && m_ctrl_hover.Enabled && m_ctrl_hover.Visible) {
                 m_ctrl_hover.OnMouseWheel(new MouseEventArgs(e.Button, e.Clicks, e.X - m_ctrl_hover.Left, pt.Y - m_ctrl_hover.Top, e.Delta));
                 return;
             }
